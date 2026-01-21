632f4559dbb9841bceeb77795b5c28154a6173ea
Eliathon <novi.sindre@gmail.com>
2026-01-21 23:40:19 +0100

    Migrated to poise for handling commands. Can now connect to voice channel

--- src/main.rs
+++ src/main.rs
@@ -3,135 +3,120 @@ mod commands;
 use std::env;
 
 use dotenvy::dotenv;
-use serenity::async_trait;
 
-use serenity::{
-    client::{Client, Context, EventHandler},
-    framework::standard::CommandResult,
-    model::{channel::Message, gateway::Ready, prelude::*},
-    prelude::GatewayIntents,
-};
+use serenity::{model::prelude::*, prelude::GatewayIntents};
 
 use songbird::SerenityInit;
 
 use crate::commands::math::*;
 
-struct Handler;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+pub struct Data;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-            println!("Received a ping command from {}", msg.author.name);
-        }
-        if msg.content.starts_with("!math") {
-            let mut input = msg.content.split_whitespace().collect::<Vec<_>>();
-            input.remove(0); // Remove !math
-            let response = parse_math_command(input)
-                .await
-                .unwrap_or_else(|e| format!("Error: {}", e));
-            if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
+type PoiseContext<'a> = poise::Context<'a, Data, Error>;
 
-        if msg.content == "!join" {
-            if let Err(why) = join_voice(&ctx, &msg).await {
-                println!("Error joining voice channel: {:?}", why);
-            }
-        }
-    }
-
-    // async fn guild_member_addition(
-    //     &self,
-    //     ctx: Context,
-    //     new_member: serenity::model::guild::Member,
-    // ) {
-    //     let welcome_message = format!("Welcome to the server, {}!", new_member.user.name);
-    //     if let Err(why) = guild_id
-    //         .system_channel(&ctx.http)
-    //         .await
-    //         .unwrap()
-    //         .say(&ctx.http, welcome_message)
-    //         .await
-    //     {
-    //         println!("Error sending welcome message: {:?}", why);
-    //     }
-    // }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
+#[poise::command(prefix_command, slash_command)]
+async fn ping(ctx: PoiseContext<'_>) -> Result<(), Error> {
+    ctx.say("Pong!").await?;
+    Ok(())
 }
 
-pub async fn join_voice(ctx: &Context, msg: &Message) -> CommandResult {
-    let guild_id = match msg.guild_id {
-        Some(guild_id) => guild_id,
-        None => {
-            msg.reply(ctx, "This command can only be used in a server")
-                .await?;
-            return Ok(());
-        }
+#[poise::command(prefix_command)]
+pub async fn math(ctx: PoiseContext<'_>, a: i32, op: String, b: i32) -> Result<(), Error> {
+    let parts = vec![a.to_string(), op, b.to_string()];
+    let parts: Vec<&str> = parts.iter().map(|s| s.as_str()).collect();
+
+    match parse_math_command(parts).await {
+        Ok(result) => ctx.say(result).await?,
+        Err(e) => ctx.say(format!("Error: {}", e)).await?,
     };
-
-    let channel_id_opt: Option<ChannelId> = guild_id.to_guild_cached(&ctx.cache).and_then(|g| {
-        g.voice_states
-            .get(&msg.author.id)
-            .and_then(|vs| vs.channel_id)
-    });
-
-    let channel_id = match channel_id_opt {
-        Some(c) => c,
-        None => {
-            msg.reply(
-                ctx,
-                "You need to be in a voice channel to use this command.",
-            )
-            .await?;
-            return Ok(());
-        }
-    };
-
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.");
-
-    match manager.join(guild_id, channel_id).await {
-        Ok(_handler_lock) => {
-            msg.reply(
-                ctx,
-                format!("Joined voice channel: {}", channel_id.mention()),
-            )
-            .await?;
-        }
-        Err(why) => {
-            msg.reply(ctx, format!("Error joining voice channel: {}", why))
-                .await?;
-        }
-    }
 
     Ok(())
 }
 
+#[poise::command(prefix_command, guild_only)]
+pub async fn join(ctx: PoiseContext<'_>) -> Result<(), Error> {
+    let serenity_ctx = ctx.serenity_context();
+    
+    let guild_id = match ctx.guild_id() {
+        Some (guild) => guild,
+        None => {
+            ctx.reply("This command can only be used in a server.").await?;
+            return Ok(());
+        }
+    };
+    
+    let channel_id_opt: Option<ChannelId> = {
+        match guild_id.to_guild_cached(&serenity_ctx.cache) {
+            Some(guild) => guild.voice_states.get(&ctx.author().id).and_then(|vs| vs.channel_id),
+            None => None,
+        }
+    };
+    
+    if guild_id.to_guild_cached(&serenity_ctx.cache).is_none() {
+        ctx.reply("Could not retrieve guild information from cache.")
+            .await?;
+        return Ok(());
+    }
+
+    let channel_id = match channel_id_opt {
+        Some(c) => c,
+        None => {
+            ctx.reply("You need to be in a voice channel to use this command.")
+                .await?;
+
+            println!("Guild ID: {}", guild_id);
+            println!("Channel: {}", ctx.channel_id());
+            println!("author: {}", ctx.author().id);
+            return Ok(());
+        }
+    };
+
+    let manager = songbird::get(serenity_ctx)
+        .await
+        .ok_or("Songbird Voice client placed in at initialisation.")?;
+
+    manager.join(guild_id, channel_id).await?;
+
+    ctx.reply(format!("Joined {}!", channel_id.mention()))
+        .await?;
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     dotenv().ok().expect("Could not load .env file");
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT
-        | GatewayIntents::GUILD_VOICE_STATES;
+        | GatewayIntents::GUILD_VOICE_STATES
+        | GatewayIntents::GUILDS;
 
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![join(), ping(), math()],
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("!".into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .setup(|_ctx, ready, _framework| {
+            Box::pin(async move {
+                println!("{} is connected!", ready.user.name);
+                Ok(Data)
+            })
+        })
+        .build();
+
+    let mut client = poise::serenity_prelude::Client::builder(&token, intents)
+        .framework(framework)
         .register_songbird()
         .await
-        .expect("Err creating client");
+        .expect("Error creating client");
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+    client.start().await?;
+    Ok(())
 }
